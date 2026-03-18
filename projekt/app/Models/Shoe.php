f485c26d7b03ba7d96369ffea4afffcd0a4acb42
Kamcio <kamciosiamsio@gmail.com>
2026-03-18 12:25:13 +0100

    Dodanie kontrolera do obsługi butów oraz utworzenie tabeli z butami

---
+++ projekt/app/Models/Shoe.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shoe extends Model
+{
+    protected $fillable = [
+        'name',
+        'brand',
+        'size',
+        'price',
+        'color',
+        'description',
+        'image',
+    ];
+}
