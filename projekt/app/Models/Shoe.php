9fb961090a786a46c97a5bf9b7663c9526576ba7
Kamcio <kamciosiamsio@gmail.com>
2026-03-18 12:45:49 +0100

    dodanie kontrolerów i modeli koszyka i finalizacji zakupu

--- projekt/app/Models/Shoe.php
+++ projekt/app/Models/Shoe.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Shoe extends Model
 {
     protected $fillable = [
-        'name',
-        'brand',
-        'size',
-        'price',
-        'color',
-        'description',
-        'image',
+        'nazwa',
+        'marka',
+        'rozmiar',
+        'cena',
+        'kolor',
+        'opis',
+        'zdjecie',
     ];
 }
