b36e2c8d71c4131c1722b018465b8d614b4dbd1c
lukagoatic <o.kubacki070@wp.pl>
2026-05-31 19:31:27 +0200

    fix

--- projekt/resources/views/layouts/app.blade.php
+++ projekt/resources/views/layouts/app.blade.php
@@ -4,10 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/admin-create.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/admin-edit.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/admin-form.css') }}"> -->
     <script src="{{ asset('js/filtrowanie.js') }}"></script>
     <script src="{{ asset('js/panele.js') }}"></script>
     
