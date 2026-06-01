8aacbf94a50a897947097f406137c481512d5d55
NickAndyKropka <mikolajandy123@gmail.com>
2026-06-01 18:04:57 +0200

    Dodanie weryfkacji email i podstron związanych z profilem użytkownika

--- projekt/resources/views/auth/forgot-password.blade.php
+++ projekt/resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,32 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+@extends('layouts.app')
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('content')
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
+<div class="min-h-screen">
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+        <h2 style="margin-bottom: 10px; text-align: center;">Przypomnienie hasła</h2>
+        <p style="margin-bottom: 20px; text-align: center; font-size: 14px;">
+            Podaj swój adres e-mail, a wyślemy Ci link do resetu hasła.
+        </p>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
+        @if (session('status'))
+            <div style="color: green; font-size: 13px; margin-bottom: 15px;">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <label for="email">E-mail</label>
+        <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+
+        @error('email')
+            <div class="text-red-600">{{ $message }}</div>
+        @enderror
+
+        <button type="submit">Wyślij link resetujący</button>
     </form>
-</x-guest-layout>
+</div>
+@endsection
